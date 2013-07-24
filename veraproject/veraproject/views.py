27bb808aca7289ff6bdee1fa007602b47330def5
Wiggin <cwiggin@live.com>
2013-07-24 15:50:12 -0700

    Moved show logic into home view.

--- veraproject/veraproject/views.py
+++ veraproject/veraproject/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from shows.models import Shows
 
 def home(request):
-	template_values = {}
+	shows = Shows.objects.all()
+
+	print shows[0].name
+
+	print shows[0].date
+
+	template_values = {
+		"shows": shows,
+	}
 	return render(request, 'home.html', template_values)
