01a67b22af2c58e7086edcb3e7b54bd4e8548d87
Wiggin <cwiggin@live.com>
2013-07-24 15:43:07 -0700

    Fixed home view.

--- veraproject/veraproject/views.py
+++ veraproject/veraproject/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from veraproject.templates import home.html
 
 def home(request):
+	template_values = {}
 	return render(request, 'home.html', template_values)
