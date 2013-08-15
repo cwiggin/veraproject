5d4725bbb1fe5e6aa81d7540b2ad225271a27039
Wiggin <cwiggin@live.com>
2013-08-15 13:09:19 -0700

    Mk 1

--- veraproject/previewsandreviews/models.py
+++ veraproject/previewsandreviews/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class preres(models.Model):
+	bandnamelink = models.CharField(max_length=1000)
+	reviewerblurb = models.CharField(max_length=200)
